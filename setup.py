f226ef000f427438e7eadb284a3c7f02da6e5533
Lakshaygoel4321 <iamlakshaygoel5990@gmail.com>
2025-01-09 15:09:35 +0530

    added eda and FeatureEnginnering

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Lakshay",
+    author_email="iamlakshaygoel5990@gmail.com",
+    packages=find_packages()
+)
